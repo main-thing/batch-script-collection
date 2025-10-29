75540d8761f49d922fc2ca1d4e925f6e69c8b761
main-thing <83129609+main-thing@users.noreply.github.com>
2025-10-30 01:58:37 +0800

    Update input.bat with emojis

--- input/input.bat
+++ input/input.bat
@@ -2,9 +2,12 @@
 :: Do not add spaces around the "=" sign, it will just unset the var
 :: Variables are treated extremely literally %_var:~0,1% returns y not "y"
 
+:: Enable emoji rendering: https://stackoverflow.com/a/72000317
+chcp 65001>nul
+
 SET /P _var="Do you want some apples? "
 IF %_var:~0,1% == y (
-	ECHO "Have some apples! (`) (`)"
+	ECHO "Have some apples! 🍎 🍎"
 ) ELSE (
 	ECHO "More for me!"
 )
